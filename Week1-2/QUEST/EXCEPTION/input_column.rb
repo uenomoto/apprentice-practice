7f3ac9f13077b8293771667756ea4a12f310df66
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-04-20 17:06:21 +0900

    add

--- Week1-2/QUEST/EXCEPTION/input_column.rb
+++ Week1-2/QUEST/EXCEPTION/input_column.rb
@@ -8,7 +8,11 @@ class InputColumn
   def self.get_number(description)
     loop do
       input = get_input(description)
-      return input_to.f if input.match(/\A-?\d+(\.\d+)?\z/)
+      # ガード節を使い 入力検証バリテーションチェック！
+      # /\A-?\d+(\.\d+)?\z/は入力が整数または小数点がマッチするかどうかをチェックする正規表現。
+      # (要するにそれ以外はマッチしないのでNoNumberErrorが出る)
+      return input.to_f if input.match(/\A-?\d+(\.\d+)?\z/) # ガード節のあとは空行を
+
       raise NoNumberError
     rescue NoNumberError => e
       puts e
@@ -18,10 +22,23 @@ class InputColumn
   def self.get_operator(description)
     loop do
       input = get_input(description)
-      return input if ['+', '-', '*', '/'].include?(input)
+      # inputで渡ってきた値が'+', '-', '*', '/'いずれかであればloopを抜けることができる。
+      # それ以外はNoMatchOperatorErrorが出る
+      return input if ['+', '-', '*', '/'].include?(input) # returnとbreakの違いについて調べる
+
       raise NoMatchOperatorError
     rescue NoMatchOperatorError => e
       puts e
     end
   end
 end
+# なぜputsではなくprintかは改行が無いため横に入力が出るため、putsは自動で改行されます。
+# ユーザーにとって見やすいインターフェイスを心掛けましょう！
+
+# さてさてここで疑問が一つ生まれました。
+# InputColumnクラスのget_inputクラスメソッドはこのクラスでしか使われておりません！
+# なぜプライベートにしないのか？
+# 今後再利用できる可能性が大いにあるからです。
+# get_inputクラスメソッドはただ単に入力を受けるだけのメソッドです。これから計算機だけでなく他の機能で使えそうです
+# 他のクラスでも再利用する可能性があるため、プライベートにしない方がいいと私は思いました。
+# これがもしこのクラス内だけに特化したメソッドで再利用することが考えにくい場合、privateメソッドにした方が良いでしょう。
